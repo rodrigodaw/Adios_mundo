da4cbe36457a0737ea9baaecbc9394ddb9266e43
rodrigodaw <Rodrijpn2@gmail.com>
2019-09-26 20:28:32 +0200

    Poniendo etiquetas

--- Adios.java
+++ Adios.java
@@ -3,7 +3,7 @@ public class Adios {
 
 	public static void main(String[] args) {
 		System.out.println("Adios Mundo");
-		System.out.println("DA FUNK?");
+		System.out.println("Harder, Better, Faster, Stronger");
 	}
 
 }
