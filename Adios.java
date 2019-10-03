0d6be19d6cd07e41fb41b76b774d174ffafe25d9
rodrigodaw <Rodrijpn2@gmail.com>
2019-10-03 18:11:29 +0200

    Experimentando con colores

--- Adios.java
+++ Adios.java
@@ -2,8 +2,10 @@
 public class Adios {
 
 	public static void main(String[] args) {
-		System.out.println("Adios Mundo");
-		System.out.println("Harder, Better, Faster, Stronger");
+		String amarillo = "\033[33m";
+		String verde = "\033[32m";
+			System.out.println(amarillo + "¡Adios mundo!");
+			System.out.println(verde + "Pero... volveré");
 	}
 
 }
